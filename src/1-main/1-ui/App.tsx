0e3a5dcb8c4153500890a317982b797601812863
FFiillaattoovv <admirator2015@yandex.ru>
2021-09-08 19:31:32 +0300

    Add provider for store transfer

--- src/1-main/1-ui/App.tsx
+++ src/1-main/1-ui/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import {HashRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import Main from './Main';
+import {store} from '../2-bll/store';
 
 function App() {
     return (
         <div className="App">
             <HashRouter>
-                {/*<Provider store={store}></Provider>*/}
-                <Main/>
+                <Provider store={store}>
+                    <Main/>
+                </Provider>
             </HashRouter>
         </div>
     );
