ac1e1f64835c23cb3f1bb1bacbb7269ffb029a8b
Bulat <m.bulat.mail@gmail.com>
2021-09-18 15:53:49 +0300

    draw sign-up jsx

--- src/2-features/1-auth/2-signUp/SignUp.tsx
+++ src/2-features/1-auth/2-signUp/SignUp.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SuperButton from "../../../1-main/1-ui/common/SuperButton/SuperButton";
+import SuperInputText from "../../../1-main/1-ui/common/SuperInputText/SuperInputText";
+import style from './SignUp.module.css'
 
 function SignUp() {
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const error = email ? '' : 'error'
+
+    const showAlert = () => {
+        if (error) {
+            alert('введите текст...')
+        } else {
+            alert(email + ' ' + password)
+        }
+    }
+
+    // const [checked, setChecked] = useState<boolean>(false)
     return (
-        <div className="App">
-            <h1>SignUp component</h1>
-            <input type={'text'}/>
-            <SuperButton/>
+        <div className={style.signUpContainer}>
+
+            <form className={style.signUpForm}>
+                <h3>Sign Up</h3>
+                <SuperInputText
+                    value={email}
+                    onChangeText={setEmail}
+                    onEnter={showAlert}
+                    error={error}
+                />
+                <SuperInputText
+                    value={password}
+                    onChangeText={setPassword}
+                    onEnter={showAlert}
+                    error={error}
+                />
+                <div>
+                    <SuperButton>Register</SuperButton>
+                    <SuperButton>Cancel</SuperButton>
+                </div>
+            </form>
         </div>
     );
 }
