ff1b962ad1fe0a51a24fc78713750cd71dd24149
Inna3112 <innula3112@gmail.com>
2021-09-19 11:49:14 +0300

    Redirect to profile and error added

--- src/2-features/1-auth/1-login/Login.tsx
+++ src/2-features/1-auth/1-login/Login.tsx
@@ -2,14 +2,18 @@ import React, {ChangeEvent, useState} from 'react';
 import SuperInputText from '../../../1-main/1-ui/common/SuperInputText/SuperInputText';
 import SuperCheckbox from '../../../1-main/1-ui/common/SuperCheckbox/SuperCheckbox';
 import SuperButton from '../../../1-main/1-ui/common/SuperButton/SuperButton';
-import { NavLink } from 'react-router-dom';
+import {NavLink, Redirect} from 'react-router-dom';
 import {PATH} from '../../../1-main/1-ui/routes/Routes';
 import style from './Login.module.css'
 import {loginSuccess} from '../../../1-main/2-bll/authReducer';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootStateType} from '../../../1-main/2-bll/store';
 
 function Login() {
     const dispatch = useDispatch()
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+    const error = useSelector<AppRootStateType, string>(state => state.auth.error)
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [rememberMe, setRememberMe] = useState(false)
@@ -20,6 +24,9 @@ function Login() {
     const loginHandler = () => {
         dispatch(loginSuccess({email, password, rememberMe}))
     }
+    if(isLoggedIn){
+        return <Redirect to={'/profile'} />
+    }
     return (
         <div className={style.loginBlock}>
             <h1>Login</h1>
@@ -29,6 +36,7 @@ function Login() {
             <NavLink to={PATH.RESET_PASSWORD}>forgot</NavLink>
             <SuperButton onClick={loginHandler}>Login</SuperButton>
             <NavLink to={PATH.SIGN_UP}>sign up</NavLink>
+            {error ? <div style={{color: 'red'}}>{error}</div> : ''}
         </div>
     );
 }
