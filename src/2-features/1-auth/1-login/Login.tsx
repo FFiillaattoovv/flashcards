6aa3b8034cdbb00c3060ee7e025d884cba283c0b
Inna3112 <innula3112@gmail.com>
2021-09-19 10:45:21 +0300

    handlers added

--- src/2-features/1-auth/1-login/Login.tsx
+++ src/2-features/1-auth/1-login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import SuperInputText from '../../../1-main/1-ui/common/SuperInputText/SuperInputText';
 import SuperCheckbox from '../../../1-main/1-ui/common/SuperCheckbox/SuperCheckbox';
 import SuperButton from '../../../1-main/1-ui/common/SuperButton/SuperButton';
@@ -6,14 +6,24 @@ import { NavLink } from 'react-router-dom';
 import {PATH} from '../../../1-main/1-ui/routes/Routes';
 
 function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [rememberMe, setRememberMe] = useState(false)
+
+    const emailHandler = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)
+    const passwordHandler = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)
+    const rememberMeHandler = () => setRememberMe(!rememberMe)
+    const loginHandler = () => {
+
+    }
     return (
         <div className="App">
             <h1>Login</h1>
-            <SuperInputText />
-            <SuperInputText />
-            <SuperCheckbox >Remember me</SuperCheckbox>
+            <SuperInputText value={email} onChange={emailHandler}/>
+            <SuperInputText value={password} onChange={passwordHandler}/>
+            <SuperCheckbox checked={rememberMe} onClick={rememberMeHandler}>Remember me</SuperCheckbox>
             <NavLink to={PATH.RESET_PASSWORD}>forgot</NavLink>
-            <SuperButton >Login</SuperButton>
+            <SuperButton onClick={loginHandler}>Login</SuperButton>
             <NavLink to={PATH.SIGN_UP}>sign up</NavLink>
         </div>
     );
