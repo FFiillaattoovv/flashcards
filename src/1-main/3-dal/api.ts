441ffc9f907287ad73837330bb67ccda205944a9
Inna3112 <innula3112@gmail.com>
2021-09-19 10:58:15 +0300

    authAPI.login added

---
+++ src/1-main/3-dal/api.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'http://localhost:7542/2.0/',
+    withCredentials: true,
+})
+
+export const authAPI = {
+    login(email: string, password: string, rememberMe: boolean) {
+        return instance.post<LoginResponseType>('auth/login', {email, password, rememberMe})
+    }
+}
+
+// types
+export type LoginResponseType = {
+    avatar: string,
+    created: string,
+    deviceTokens: DeviceTokenType [],
+    email: string,
+    isAdmin: boolean,
+    name: string,
+    publicCardPacksCount: number,
+    rememberMe: boolean,
+    token: string,
+    tokenDeathTime: number,
+    updated: string,
+    verified: boolean,
+    __v: number,
+    _id: string,
+}
+type DeviceTokenType = {
+    _id: string,
+    device: string,
+}
