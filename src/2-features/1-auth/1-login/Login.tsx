8ae607dd3b048c27c4a0013bdfcf6ab041641253
Inna3112 <innula3112@gmail.com>
2021-09-19 10:39:07 +0300

    jsx added

--- src/2-features/1-auth/1-login/Login.tsx
+++ src/2-features/1-auth/1-login/Login.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import SuperInputText from '../../../1-main/1-ui/common/SuperInputText/SuperInputText';
+import SuperCheckbox from '../../../1-main/1-ui/common/SuperCheckbox/SuperCheckbox';
+import SuperButton from '../../../1-main/1-ui/common/SuperButton/SuperButton';
+import { NavLink } from 'react-router-dom';
+import {PATH} from '../../../1-main/1-ui/routes/Routes';
 
 function Login() {
     return (
         <div className="App">
-            <h1>Login component</h1>
+            <h1>Login</h1>
+            <SuperInputText />
+            <SuperInputText />
+            <SuperCheckbox >Remember me</SuperCheckbox>
+            <NavLink to={PATH.RESET_PASSWORD}>forgot</NavLink>
+            <SuperButton >Login</SuperButton>
+            <NavLink to={PATH.SIGN_UP}>sign up</NavLink>
         </div>
     );
 }
