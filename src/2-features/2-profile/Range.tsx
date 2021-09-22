230d53baf48b639d4a587f0ae334ccfdc9794ef0
FFiillaattoovv <admirator2015@yandex.ru>
2021-09-22 17:00:46 +0300

    Add test data for Range component and  render them

--- src/2-features/2-profile/Range.tsx
+++ src/2-features/2-profile/Range.tsx
@@ -1,11 +1,13 @@
 import classes from "./Profile.module.css";
-import React from "react";
+import React, {useState} from "react";
 
 export function Range() {
+    const [value, setValue] = useState(0)
     return (
         <div>
             <span className={classes.numberCards}>Number of cards</span>
-            <input type="range" name="range" min="0" max="100" value="50"/>
+            <span>{value}</span>
+            <input type="range" name="range" min="0" max="100" value={value} onChange={e => setValue(+e.currentTarget.value)}/>
         </div>
     )
 }
