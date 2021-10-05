a2c56373ed0abb04f53fff0257a1701120b7baec
ivankishko <mrivankishko@gmail.com>
2021-10-05 14:11:30 +0300

    routes

--- src/1-main/1-ui/Main.tsx
+++ src/1-main/1-ui/Main.tsx
@@ -4,6 +4,7 @@ import Routes from './routes/Routes';
 import Login from '../../2-features/1-auth/1-login/Login';
 import {AppRootStateType} from '../2-bll/store';
 import {useSelector} from 'react-redux';
+import UnauthorizedRoutes from "./routes/UnauthorizedRoutes";
 
 function Main() {
     const isAuth = useSelector<AppRootStateType, boolean>(state => state.auth.isAuth)
@@ -19,7 +20,7 @@ function Main() {
                     </div>
                     :
                     <div>
-                        <Login/>
+                        <UnauthorizedRoutes/>
                     </div>
             }
         </div>
---
+++ src/1-main/1-ui/routes/UnauthorizedRoutes.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Redirect, Route, Switch} from 'react-router-dom';
+import Error404 from '../../../2-features/1-auth/2-signUp/sign-up-common/Error404/Error404';
+import Login from '../../../2-features/1-auth/1-login/Login';
+import SignUp from '../../../2-features/1-auth/2-signUp/SignUp';
+import ResetPassword from '../../../2-features/restore/1-resetPassword/ResetPassword';
+import NewPassword from '../../../2-features/restore/2-newPassword/NewPassword';
+import CheckEmail from "../../../2-features/restore/1-resetPassword/CheckEmail";
+import {PATH} from "./Routes";
+
+function UnauthorizedRoutes() {
+    return (
+        <div>
+            <Switch>
+                <Route path={'/'} exact render={() => <Redirect to={PATH.LOGIN}/>}/>
+                <Route path={PATH.SIGN_UP} render={() => <SignUp/>}/>
+                <Route path={PATH.RESET_PASSWORD} render={() => <ResetPassword/>}/>
+                <Route path={PATH.CHECK_EMAIL} render={() => <CheckEmail/>}/>
+                <Route path={PATH.NEW_PASSWORD} render={() => <NewPassword/>}/>
+                <Route path={PATH.LOGIN} render={() => <Login/>}/>
+                <Route render={() => <Error404/>}/>
+            </Switch>
+        </div>
+    )
+}
+
+export default UnauthorizedRoutes
